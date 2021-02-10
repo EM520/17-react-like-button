3a84c8442ae32538acc68c1451c04d8126db1c94
punchcode <punchcode@punchcodes-MacBook-Pro.local>
2021-02-09 23:07:14 -0500

    updatd app.js app.css

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState, useEffect} from "react"
+
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [text,setText] = useState("likes")
+  // useEffect(() => {
+  //   count==1?setText("like"):setText("likes")
+  // }, [text]);
+  // function handleClick() {
+  //   setText(count+1);
+  // }
+  return (
+    <div className="App">
+      {/* <button id="btn" onClick={handleClick}>{text}</button> */}
+      <button id="btn" onClick={()=>setCount(count+1) }>{count}{text}</button>
+    </div>
+  );
+}
+
+export default App;
